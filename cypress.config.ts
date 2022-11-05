b02e57f63a30220f950dda1686ca372a6ed6ccc6
Dongwook Kim <dongwook227@gmail.com>
2022-11-05 14:08:26 -0400

    install cypress (#3)

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    excludeSpecPattern: [
+      '**/cypress/e2e/1-getting-started',
+      '**/cypress/e2e/2-advanced-examples',
+    ],
+  },
+
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
+  },
+})
